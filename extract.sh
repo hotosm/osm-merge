05cb96fd93d9e737b1f6c00fb2320b2147ca68c9
Rob Savoye <rob@senecass.com>
2022-02-15 11:27:33 -0700

    Add note abpout how the id field gets ignored

--- extract.sh
+++ extract.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2022   Humanitarian OpenStreetMap Team
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+#
+# This script queries the Tasking Manager database to get the project boundary,
+# and the boundaries of all the tasks. Note that when producing GeoJson output,
+# the 'id' field gets filtered out by ogr2ogr, so we rename it. Otherwise there
+# are no fields in the file used to specify how the large data file gets split.
 project=$1
 
-ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-project.geojson PG:"dbname=tmsnap" -nln "project" -sql "SELECT id AS pid,ST_AsText(geometry) FROM projects WHERE id=${project}"
+ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-project.geojson PG:"dbname=tmsnap" -nln "project" -sql "SELECT id AS id,ST_AsText(geometry) FROM projects WHERE id=${project}"
 
 ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-tasks.geojson PG:"dbname=tmsnap" -sql "SELECT tasks.id AS tid,projects.id AS pid,ST_AsText(tasks.geometry) FROM tasks,projects WHERE tasks.project_id=${project} AND projects.id=${project}"
