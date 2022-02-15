b928c4e8c9441e6c01285f47c01ce9ff443da196
Rob Savoye <rob@senecass.com>
2022-02-15 12:32:33 -0700

    Write a consistent layer name

--- extract.sh
+++ extract.sh
@@ -24,6 +24,6 @@
 # are no fields in the file used to specify how the large data file gets split.
 project=$1
 
-ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-project.geojson PG:"dbname=tmsnap" -nln "project" -sql "SELECT id AS id,ST_AsText(geometry) FROM projects WHERE id=${project}"
+ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-project.geojson PG:"dbname=tmsnap" -nln "tmproject" -sql "SELECT id AS pid,ST_AsText(geometry) FROM projects WHERE id=${project}"
 
-ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-tasks.geojson PG:"dbname=tmsnap" -sql "SELECT tasks.id AS tid,projects.id AS pid,ST_AsText(tasks.geometry) FROM tasks,projects WHERE tasks.project_id=${project} AND projects.id=${project}"
+ogr2ogr -s_srs "EPSG:4326" -t_srs "EPSG:4326" -progress -overwrite -f "GeoJSON" ${project}-tasks.geojson PG:"dbname=tmsnap" -nln "tmproject" -sql "SELECT projects.id AS pid,tasks.id AS tid,ST_AsText(tasks.geometry) FROM tasks,projects WHERE tasks.project_id=${project} AND projects.id=${project}"
