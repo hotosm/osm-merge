fcfee3f95e939d61714b6819942501cedf760fee
Rob Savoye <rob.savoye@hotosm.org>
2022-03-07 11:58:44 -0700

    Use hotstuff instead of duplicate code

--- conflator.py
+++ conflator.py
@@ -22,102 +22,201 @@ import getopt
 from sys import argv
 # import underpass
 import os
+import epdb
+import sys
 from osgeo import ogr
 # from geojson import Feature, Polygon, LineString, MultiLineString
-# from progress.bar import Bar, PixelBar
-from progress.spinner import PixelSpinner
+from progress.bar import Bar, PixelBar
+# from progress.spinner import PixelSpinner
+import hotstuff
+from codetiming import Timer
 
-options = dict()
-options["schema"] = "pgsnapshot";
-options["osmin"] = None;
-options["bldin"] = None;
-options["tmin"] = None;
-options["tmproj"] = None;
 
-def usage():
-    out = """
-    --help(-h)      Get command line options
-    --tmin(-t)      file or Tasking Manazger database URL (host@user:password/database)"
-    --osmin(-o)     OSM Database URL (host@user:password/database)"
-    --bldin(-b)     Building Database URL (host@user:password/database)"
-    --project(-p)   Tasking Manager project ID to get boundary from a database
-    --boundary(-b)  Specify a polygon as a boundary
-    --schema(-s)    OSM database schema (pgsnapshot, ogr2ogr, osm2pgsql) defaults to \"%s\"
-    """ % (options['schema'])
+# All command line options
+options = hotstuff.CommonOptions(argv)
+
+def usage(options):
+    out = options.usage()
+    out += """
+    Examples:
+    The command: ./conflator.py -t tmsnap -p 8345 -b pg:kenya_foot -o pg:Kenya
+    Reads from 3 data sources. The first one is a snapshot of the Tasking Manager database,
+    and we want to use project 8345 as the boundary. Since the data files are huge, it's
+    more efficient to work on subsets of all the data.
+
+    The other two are prefixed with "pg", which defines them as a database URL instead of a file
+
+    """
     print(out)
     quit()
 
-if len(argv) <= 1:
-    usage()
 
-try:
-    (opts, val) = getopt.getopt(argv[1:], "h,t:,o:,b:,p:,b:,s:",
-        ["help", "tmin", "osmin", "bldin", "project", "boundary", "schema"])
-except getopt.GetoptError as e:
-    logging.error('%r' % e)
-    usage(argv)
+timer = Timer()
+
+# def findDuplicatesDatabase(self, osm, newbld):
+#     """Find duplicate buildings between two databases"""
+#     sql = "SELECT ST_Area(ST_Transform(ST_INTERSECTION(g1.way, g2.way), 2167)),g1.osm_id,ST_Area(ST_Transform(g1.way, 2167)),g2.osm_id,ST_Area(ST_Transform(g2.way, 2167)) FROM boundary AS g1, boundary AS g2 WHERE ST_OVERLAPS(g1.way, g2.way);"
+#     pass
+
+# Get the boundary of the data to process
+if options.get("boundary"):
+    rows = hotstuff.getProjectBoundary(options)
+    if rows is None:
+        logging.error("Boundary file should be a single polygon of a Tasking Manager project or an administrative boundary")
+        quit()
+else:
+    rows = None
+
+footprints = options.get('footprints')
+if footprints[0:3] == "pg:":
+    logging.info("Opening database connection to: %s" % footprints)
+    connect = "PG: dbname=" + footprints[3:]
+    if options.get('dbhost') != "localhost":
+        connect += " host=" + options.get('dbhost')
+        bldin = ogr.Open(connect)
+else:
+    logging.info("Opening buildings data file: %s" % footprints)
+    bldin = ogr.Open(footprints)
+buildings = bldin.GetLayer()
+if buildings:
+    logging.info("%d Buildings in %s" % (buildings.GetFeatureCount(), footprints))
+else:
+    logging.error("No buildings found in %s" % footprints)
     quit()
 
-for (opt, val) in opts:
-    if opt == '--help' or opt == '-h':
-        usage()
-    elif opt == "--osmin" or opt == '-o':
-        options['osmin'] = val
-    elif opt == "--project" or opt == '-p':
-        options['tmproj'] = val
-    elif opt == "--tmin" or opt == '-t':
-        options['tmin'] = val
-    elif opt == "--boundary" or opt == '-b':
-        options['boundary'] = val
-    elif opt == "--buildings" or opt == '-b':
-        options['bldin'] = val
-    elif opt == "--schema" or opt == '-s':
-        options['schema'] = val
+if rows:
+    timer.start()
+    buildings.ResetReading()
+    buildings.SetSpatialFilter(rows[0]['boundary'])
+    logging.debug("%d features after filtering" % (buildings.GetFeatureCount()))
+    timer.stop()
 
-#if options['osmdb'] is None and options['blddb'] is None and options['tmdb']:
-#    usage()
+osmdata = options.get('osmdata')
+if osmdata[0:3] == "pg:":
+    logging.info("Opening database connection to: %s" % osmdata)
+    connect = "PG: dbname=" + osmdata[3:]
+    if options.get('dbhost') != "localhost":
+        connect += " host=" + options.get('dbhost')
+        osmin = ogr.Open(connect)
+else:
+    logging.info("Opening OSM data file: %s" % osmdata)
+    osmin = ogr.Open(osmdata)
+osm = osmin.GetLayer()
+if osm:
+    logging.info("%d OSM Features in %s" % (osm.GetFeatureCount(), osmdata))
+else:
+    logging.error("No features found in %s" % osmdata)
+    quit()
 
-#
-# Get the project boundary from the Tasking Manager database
-#
+bfields = buildings.GetLayerDefn()
 
-def getProjectBoundary():
-    # FIXME: handle actual URL, don't assume localhost auth
-    if options['tmproj'] is not None:
-        connect = "PG: dbname=" + options['tmin']
-        tmin = ogr.Open(connect)
-    else:
-        tmin = ogr.Open(options['tmin'])
+if rows:
+    timer.start()
+    osm.ResetReading()
+    # FIXME: This drops most of the buildings for some reason
+    #osm.SetAttributeFilter("tags->>\'building\'=\'yes\'")
+    osm.SetSpatialFilter(rows[0]['boundary'])
+    timer.stop()
+    logging.debug("%d OSM features after filtering" % (osm.GetFeatureCount()))
 
-    if options['tmproj'] is not None:
-        sql = "SELECT id AS pid,ST_AsText(geometry) FROM projects WHERE id=" + str(options['tmproj'])
-        layer = tmin.ExecuteSQL(sql)
-        print(sql)
-    elif options['tmin'] is not None:
-        layer = tmin.GetLayer("tmproject")
-    else:
-        logging.error("Need to specify input data!")
-    if layer is None:
-        logging.error("No such project in the Tasking Manager database")
-        return None
+# Output is GeoJson
+drv = ogr.GetDriverByName("GeoJSON")
 
-    # Create a bounding box. since we want a rectangular area to extract to fit a monitor window
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    extent = layer.GetExtent()
-    ring.AddPoint(extent[0],extent[2])
-    ring.AddPoint(extent[1], extent[2])
-    ring.AddPoint(extent[1], extent[3])
-    ring.AddPoint(extent[0], extent[3])
-    ring.AddPoint(extent[0],extent[2])
-    poly = ogr.Geometry(ogr.wkbPolygon)
-    poly.AddGeometry(ring)
+# Driver doesn't support deleting files, so handle it ourself
+file = options.get('prefix') + "-test.geojson"
+if os.path.exists(file):
+    drv.DeleteDataSource(file)
 
-    return poly
+# Create the output file
+outfile  = drv.CreateDataSource(file)
+outlayer = outfile.CreateLayer("buildings", geom_type=ogr.wkbPolygon)
+fields = outlayer.GetLayerDefn()
 
-#
-#
-#
-# Get the boundary of the data to process
-boundary = getProjectBoundary()
-print(boundary)
+newid = ogr.FieldDefn("id", ogr.OFTInteger)
+outlayer.CreateField(newid)
+bld = ogr.FieldDefn("building", ogr.OFTString)
+outlayer.CreateField(bld)
+src = ogr.FieldDefn("source", ogr.OFTString)
+outlayer.CreateField(src)
+status = ogr.FieldDefn("status", ogr.OFTString)
+outlayer.CreateField(status)
 
+# logging.info("Looking for unique buildings")
+# timer.start()
+# lyr1 = buildings
+# lyr2 = osm
+# lyr = lyr1.SymDifference(lyr2, outlayer)
+# logging.debug("%d features in output dataset" % lyr.GetFeatureCount())
+# timer.stop()
+# quit()
+
+if rows:
+    # timer.start()
+    sql = "SELECT geom FROM ways_poly WHERE tags->>\'building\' IS NOT NULL and ST_Within(geom, ST_GeomFromEWKT(\'SRID=4326;%s\'))" % rows[0]['boundary'].ExportToWkt()
+    print(sql)
+    layer1 = osmin.ExecuteSQL(sql)
+    # layer1 = osm
+    # layer1.SetSpatialFilter(rows[0]['boundary'])
+    logging.debug("%d features in output dataset" % layer1.GetFeatureCount())
+    # timer.stop()
+
+# There is no boundary file, which is used to conflate two small datasets that have
+# already been created with the same boundary
+# if rows is None:
+#     logging.debug("Using SymDifference")
+#     timer.start()
+#     lyr = buildings.SymDifference(osm, outlayer)
+#     logging.info("Wrote output file \'%s\'" % file)
+#     # for feature in lyr:
+#     #     makeFeature(id, fields, msgeom)
+#     timer.stop()
+#     quit()
+
+#epdb.st()
+# bar = PixelSpinner('Processing...', max=layer1.GetFeatureCount())
+bar = Bar('Processing...', max=buildings.GetFeatureCount())
+
+id = 1
+counter = 0
+for msbld in buildings:
+    bar.next()
+    counter += 1
+    msgeom = msbld.GetGeometryRef()
+    mswkt = msgeom.ExportToWkt()
+    dup = False
+    for osmbld in osm:
+        # print(osmbld.GetGeometryRef().ExportToWkt())
+        osmgeom = osmbld.GetGeometryRef()
+        intersect = osmgeom.Intersects(msgeom)
+        touches = osmgeom.Touches(msgeom)
+        overlap = osmgeom.Overlaps(msgeom)
+
+        # print("GDAL: %r, %r, %r" % (intersect, touches, overlap))
+        if intersect or overlap or touches:
+            #         index = osmbld.GetFieldIndex('osm_id')
+            # msbld.DumpReadable()
+            logging.debug("Found intersecting buildings: %r" % counter)
+            dup = True
+        mscnt = msgeom.Centroid()
+        osmcnt = osmgeom.Centroid()
+        hit1 = osmgeom.Contains(mscnt)
+        hit2 = msgeom.Contains(osmcnt)
+        if hit1 and hit2:
+            print("Found duplicate buildings %r, %r, %r" % (hit1, hit2, counter))
+            dup = True
+            break
+    # If we have a duplicate, don't write anything to the outlayer
+    if dup:
+        continue
+
+    logging.debug("New building ID: %s" % id)
+    feature = hotstuff.makeFeature(id, fields, msgeom)
+    outlayer.CreateFeature(feature)
+    feature.Destroy()
+    id += 1
+
+logging.info("Wrote output file \'%s\'" % file)
+
+outfile.Destroy()
+#buildings.Destroy()
+#osm.Destroy()
