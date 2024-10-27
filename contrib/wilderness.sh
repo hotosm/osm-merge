5fce1196157a66232d20e52b300db68aedfd59be
Rob Savoye <rob@senecass.com>
2024-10-27 13:54:01 -0400

    fix: Add data file for all US wilderness areas

---
+++ contrib/wilderness.sh
@@ -0,0 +1,270 @@
+wildcolorado="Sarvis_Creek_Wilderness \
+Neota_Wilderness \
+Uncompahgre_Wilderness \
+Spanish_Peaks_Wilderness \
+Hermosa_Creek_Wilderness \
+Byers_Peak_Wilderness \
+Comanche_Peak_Wilderness \
+Fossil_Ridge_Wilderness \
+Hunter-Fryingpan_Wilderness \
+Lost_Creek_Wilderness \
+Mount_Sneffels_Wilderness \
+Mount_Zirkel_Wilderness \
+Never_Summer_Wilderness \
+West_Elk_Wilderness \
+Eagles_Nest_Wilderness \
+Cache_La_Poudre_Wilderness \
+South_San_Juan_Wilderness"
+
+wildutah="Pine_Valley_Mountain_Wilderness \
+Dark_Canyon_Wilderness \
+Ashdown_Gorge_Wilderness \
+Box-Death_Hollow_Wilderness \
+Cottonwood_Forest_Wilderness \
+Deseret_Peak_Wilderness \
+Lone_Peak_Wilderness \
+Mount_Naomi_Wilderness \
+Mount_Nebo_Wilderness \
+Mount_Olympus_Wilderness \
+Mount_Timpanogos_Wilderness \
+Twin_Peaks_Wilderness \
+Wellsville_Mountain_Wilderness \
+Nelson_Mountain_Wilderness"
+
+wildwyoming="Huston_Park_Wilderness \
+Encampment_River_Wilderness \
+Platte_River_Wilderness \
+Savage_Run_Wilderness \
+Bridger_Wilderness \
+Fitzpatrick_Wilderness \
+Teton_Wilderness \
+Washakie_Wilderness \
+Cloud_Peak_Wilderness \
+Gros_Ventre_Wilderness \
+Jedediah_Smith_Wilderness \
+North_Absaroka_Wilderness \
+Popo_Agie_Wilderness \
+Winegar_Hole_Wilderness"
+
+wildarizona="Red_Rock-Secret_Mountain_Wilderness \
+West_Clear_Creek_Wilderness \
+Chiricahua_Wilderness \
+Escudilla_Wilderness \
+Galiuro_Wilderness \
+Hellsgate_Wilderness \
+Pajarita_Wilderness \
+Salome_Wilderness \
+Superstition_Wilderness \
+Woodchute_Wilderness \
+Apache_Creek_Wilderness \
+Bear_Wallow_Wilderness \
+Castle_Creek_Wilderness \
+Cedar_Bench_Wilderness \
+Fossil_Springs_Wilderness \
+Four_Peaks_Wilderness \
+Granite_Mountain_Wilderness \
+Juniper_Mesa_Wilderness \
+Kachina_Peaks_Wilderness \
+Kanab_Creek_Wilderness \
+Miller_Peak_Wilderness \
+Mount_Baldy_Wilderness \
+Mt._Wrightson_Wilderness \
+Munds_Mountain_Wilderness \
+Pine_Mountain_Wilderness \
+Pusch_Ridge_Wilderness \
+Rincon_Mountain_Wilderness \
+Santa_Teresa_Wilderness \
+Sierra_Ancha_Wilderness \
+Strawberry_Crater_Wilderness \
+Wet_Beaver_Wilderness \
+Salt_River_Canyon_Wilderness"
+
+wildidaho="Cecil_D_Andrus_White_Clouds_Wilderness \
+Hemingway-Boulders_Wilderness \
+Gospel_Hump_Wilderness \
+Sawtooth_Wilderness \
+Jim_McClure-Jerry_Peak_Wilderness"
+
+wildmontana="Rattlesnake_Wilderness \
+Gates_of_the_Mountains_Wilderness \
+Great_Bear_Wilderness \
+Mission_Mountains_Wilderness \
+Welcome_Creek_Wilderness"
+
+wildoregon="Wenaha-Tucannon_Wilderness \
+Menagerie_Wilderness \
+Badger_Creek_Wilderness \
+Mount_Hood_Wilderness \
+Roaring_River_Wilderness \
+Mark_O._Hatfield_Wilderness \
+Black_Canyon_Wilderness \
+Boulder_Creek_Wilderness \
+Bridge_Creek_Wilderness \
+Clackamas_Wilderness \
+Cummins_Creek_Wilderness \
+Drift_Creek_Wilderness \
+Eagle_Cap_Wilderness \
+Gearhart_Mountain_Wilderness \
+Kalmiopsis_Wilderness \
+Middle_Santiam_Wilderness \
+Mill_Creek_Wilderness \
+Mountain_Lakes_Wilderness \
+Opal_Creek_Wilderness \
+Rock_Creek_Wilderness \
+Salmon-Huckleberry_Wilderness \
+Strawberry_Mountain_Wilderness \
+Waldo_Lake_Wilderness \
+Copper_Salmon_Wilderness \
+Grassy_Knob_Wilderness \
+North_Fork_Umatilla_Wilderness \
+Bull_of_the_Woods_Wilderness"
+
+wildwashington="Boulder_River_Wilderness \
+Mount_Baker_Wilderness \
+Clearwater_Wilderness \
+Salmo-Priest_Wilderness \
+Buckhorn_Wilderness \
+Lake_Chelan-Sawtooth_Wilderness \
+Noisy-Diobsud_Wilderness \
+Wild_Sky_Wilderness \
+Wonder_Mountain_Wilderness \
+Colonel_Bob_Wilderness \
+Mount_Skokomish_Wilderness \
+Norse_Peak_Wilderness \
+Tatoosh_Wilderness \
+The_Brothers_Wilderness \
+Glacier_View_Wilderness \
+Indian_Heaven_Wilderness \
+Mount_Adams_Wilderness \
+Trapper_Creek_Wilderness"
+
+wildcalifornia="Cahuilla_Mountain_Wilderness \
+San_Jacinto_Wilderness \
+Surprise_Canyon_Wilderness \
+San_Mateo_Canyon_Wilderness \
+Caribou_Wilderness \
+Chanchelulla_Wilderness \
+Emigrant_Wilderness \
+Hauser_Wilderness \
+Ishi_Wilderness \
+Kaiser_Wilderness \
+Russian_Wilderness \
+Sanhedrin_Wilderness \
+Magic_Mountain_Wilderness \
+San_Gabriel_Wilderness \
+Sheep_Mountain_Wilderness \
+Owens_River_Headwaters_Wilderness \
+South_Fork_San_Jacinto_Wilderness \
+Agua_Tibia_Wilderness \
+Bucks_Lake_Wilderness \
+Castle_Crags_Wilderness \
+Chumash_Wilderness \
+Dinkey_Lakes_Wilderness \
+Garcia_Wilderness \
+Granite_Chief_Wilderness \
+Jennie_Lakes_Wilderness \
+Marble_Mountain_Wilderness \
+Matilija_Wilderness \
+Mt._Shasta_Wilderness \
+Pine_Creek_Wilderness \
+Sespe_Wilderness \
+South_Warner_Wilderness \
+Thousand_Lakes_Wilderness \
+Ventana_Wilderness \
+Dick_Smith_Wilderness \
+San_Rafael_Wilderness \
+Silver_Peak_Wilderness \
+Mount_Lassic_Wilderness \
+North_Fork_Wilderness \
+Pleasant_View_Ridge_Wilderness \
+Snow_Mountain_Wilderness"
+
+wildnewmexico="Columbine-Hondo_Wilderness \
+Gila_Wilderness \
+Withington_Wilderness \
+Aldo_Leopold_Wilderness \
+Apache_Kid_Wilderness \
+Blue_Range_Wilderness \
+Capitan_Mountains_Wilderness \
+Cruces_Basin_Wilderness \
+Dome_Wilderness \
+Latir_Peak_Wilderness \
+Manzano_Mountain_Wilderness \
+Sandia_Mountain_Wilderness \
+Wheeler_Peak_Wilderness \
+White_Mountain_Wilderness \
+San_Pedro_Parks_Wilderness"
+
+wildsouthdakota="Black_Elk_Wilderness"
+
+wildmichigan="Delirium_Wilderness \
+Mackinac_Wilderness \
+McCormick_Wilderness \
+Sylvania_Wilderness \
+Big_Island_Lake_Wilderness \
+Rock_River_Canyon_Wilderness \
+Horseshoe_Bay_Wilderness \
+Nordhouse_Dunes_Wilderness \
+Round_Island_Wilderness \
+Sturgeon_River_Gorge_Wilderness"
+
+wildtennesse="Bald_River_Gorge_Wilderness \
+Big_Laurel_Branch_Wilderness \
+Little_Frog_Mountain_Wilderness \
+Upper_Bald_River_Wilderness \
+Big_Frog_Wilderness \
+Citico_Creek_Wilderness \
+Gee_Creek_Wilderness \
+Pond_Mountain_Wilderness \
+Sampson_Mountain_Wilderness \
+Unaka_Mountain_Wilderness"
+
+wildalaska="Petersburg_Creek-Duncan_Salt_Chuck_Wilderness \
+Kuiu_Wilderness \
+Stikine-LeConte_Wilderness \
+Misty_Fjords_National_Monument_Wilderness \
+Chuck_River_Wilderness \
+Coronation_Island_Wilderness \
+Endicott_River_Wilderness \
+Karta_River_Wilderness \
+Maurelle_Islands_Wilderness \
+Pleasant/Lemusurier/Inian_Islands_Wilderness \
+Russell_Fjord_Wilderness \
+South_Baranof_Wilderness \
+South_Etolin_Wilderness \
+Tebenkof_Bay_Wilderness \
+Warren_Island_Wilderness \
+West_Chichagof-Yakobi_Wilderness \
+Kootznoowoo_Wilderness \
+Tracy_Arm-Fords_Terror_Wilderness \
+South_Prince_of_Wales_Wilderness"
+
+wildnevada="Jarbidge_Wilderness \
+Shellback_Wilderness \
+Wovoka_Wilderness \
+Santa_Rosa-Paradise_Peak_Wilderness \
+White_Pine_Range_Wilderness \
+Alta_Toquima_Wilderness \
+Bald_Mountain_Wilderness \
+Boundary_Peak_Wilderness \
+Currant_Mountain_Wilderness \
+East_Humboldt_Wilderness \
+Grant_Range_Wilderness \
+High_Schells_Wilderness \
+Mt._Rose_Wilderness \
+Quinn_Canyon_Wilderness \
+Red_Mountain_Wilderness \
+Ruby_Mountains_Wilderness \
+Table_Mountain_Wilderness"
+
+wildkentucky="Beaver_Creek_Wilderness \
+Clifty_Wilderness"
+
+wildvermont="George_D_Aiken_Wilderness \
+Big_Branch_Wilderness \
+Bristol_Cliffs_Wilderness \
+Joseph_Battell_Wilderness \
+Lye_Brook_Wilderness \
+Peru_Peak_Wilderness \
+Breadloaf_Wilderness \
+Glastenbury_Wilderness"
